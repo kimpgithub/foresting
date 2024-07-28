610e4bdc8179c0d1e80922b3c578593c6c48fa81
corea <coreashield@gmail.com>
2024-07-28 17:15:39 +0900

    로그인페이지 디자인 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -97,4 +97,7 @@ dependencies {
     implementation("com.google.android.gms:play-services-location:21.3.0")
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+
+    //lottie
+    implementation ("com.airbnb.android:lottie-compose:6.4.1")
 }
--- app/src/main/java/com/example/san_lim/screens/login/LoginScreen.kt
+++ app/src/main/java/com/example/san_lim/screens/login/LoginScreen.kt
@@ -1,82 +1,135 @@
 package com.example.san_lim.screens.login
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.san_lim.loginUser
 import com.example.san_lim.signUpUser
+import kotlinx.coroutines.delay
+import com.example.san_lim.R
 @Composable
 fun LoginScreen(navController: NavHostController) {
     var email by remember { mutableStateOf("test@intel.com") }
     var password by remember { mutableStateOf("123456") }
-    var isLogin by remember { mutableStateOf(true) } // true for login, false for sign up
+    var isLogin by remember { mutableStateOf(true) }
 
-    Column(
+    val backgroundPainter: Painter = painterResource(id = R.drawable.background)
+
+    Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center
+            .background(Color(0xFFFFFFFF))
     ) {
-        OutlinedTextField(
-            value = email,
-            onValueChange = { email = it },
-            label = { Text("Email") },
-            modifier = Modifier.fillMaxWidth()
+        Image(
+            painter = backgroundPainter,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
         )
-        Spacer(modifier = Modifier.height(8.dp))
-        OutlinedTextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text("Password") },
-            modifier = Modifier.fillMaxWidth()
-        )
-        Spacer(modifier = Modifier.height(16.dp))
 
-        if (isLogin) {
-            Button(
-                onClick = { loginUser(email, password, navController) },
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text("Login")
-            }
-            Spacer(modifier = Modifier.height(8.dp))
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(80.dp))
+
             Text(
-                text = "Don't have an account? Sign up",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp)
-                    .clickable { isLogin = false }
+                text = "Sanlim",
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold
             )
-        } else {
-            Button(
-                onClick = { signUpUser(email, password, navController) },
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text("Sign Up")
-            }
-            Spacer(modifier = Modifier.height(8.dp))
             Text(
-                text = "Already have an account? Login",
+                text = "우리 산림의 재미를 더하다",
+                fontSize = 14.sp,
+                color = Color.Gray
+            )
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(160.dp))
+
+            Text(
+                text = "로그인",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("이메일") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("비밀번호") },
+                visualTransformation = PasswordVisualTransformation(), // 비밀번호 필드에 변환기 추가
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = { if (isLogin) loginUser(email, password, navController) else signUpUser(email, password, navController) },
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp)
-                    .clickable { isLogin = true }
+                    .width(280.dp)
+                    .height(50.dp),
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color.Black)
+            ) {
+                Text(text = if (isLogin) "로그인" else "가입하기", fontSize = 18.sp, color = Color.White)
+            }
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Text(
+                text = if (isLogin) "아이디가 없으신가요? 회원가입" else "이미 계정이 있으신가요? 로그인",
+                color = Color.Blue,
+                modifier = Modifier.clickable { isLogin = !isLogin }
             )
         }
     }
